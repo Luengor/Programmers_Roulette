6e32e6692c06990e873dc0da2a551f5ddf5bf7a2
Luengor <aluengor0001@gmail.com>
2022-02-23 00:17:04 +0100

    added, something

--- main.py
+++ main.py
@@ -6,24 +6,29 @@ import time
 if platform.startswith("linux"):
     shutdown = "shutdown now"
 elif platform in ["win32", "cygwin"]:
-    shutdown = "shutdown /s"
+    shutdown = "shutdown /p /f"
 
 shut = random.randint(0, 6)
 
 player = True
 for drum in range(6):
     if player:
-        input(f"drum {drum+1}.")
+        input(f"drum {drum+1}, press enter to drum")
+        time.sleep(1)
         if drum == shut:
             print("o7")
+            time.sleep(0.5)
             subprocess.run(shutdown, shell=True)
+            break
         else:
             print("no shut")
     else:
-        print(f"drum {drum+1}.")
-        time.sleep(3)
+        time.sleep(2)
+        print(f"drum {drum+1}, computer's turn")
+        time.sleep(2)
         if drum == shut:
             print("You win!!")
+            break
         else:
             print("no shut")
     
